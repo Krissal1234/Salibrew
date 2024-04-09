2565fe2ed28eb6687fd53411f27cad57092802e5
= <salibakris03@gmail.com>
2024-04-09 23:17:35 +0200

    added some more to todo, all that is left is to implement save to file

--- src/commands/todo.rs
+++ src/commands/todo.rs
@@ -1,5 +1,9 @@
+use std::{fs, io::{self, ErrorKind}, path::Path};
+
 use serde::{Serialize,Deserialize};
 use clap::Args;
+
+use crate::commands::todo;
 #[derive(Debug,Args)]
 pub struct TodoCommands{
     #[arg(short,long)]
@@ -20,8 +24,33 @@ pub struct TodoItem {
     pub completed: bool, 
 }
 
-// impl TodoItem {
-//     pub fn add_new(description: &str) -> Result<()> {
+impl TodoItem {
 
-//     }
-// }
+    fn load_todos() -> Result<Vec<TodoItem>,io::Error> {
+        let file_path = "../data_stores/todo.json";
+        if Path::new(file_path).exists() {
+            let data = fs::read_to_string(file_path)?;
+            serde_json::from_str(&data)
+            .map_err(|e| io::Error::new(ErrorKind::InvalidData, e.to_string()))
+        } else {
+            Err(io::Error::new(ErrorKind::NotFound, "Todo file not found"))
+        }
+    }
+
+    pub fn add_new( description: &str) -> Result<(),io::Error> {
+    let mut todos = match TodoItem::load_todos() {
+        Ok(data) => data,
+        Err(e) => return Err(e),
+    };
+    todos.push(TodoItem {
+        id: 2,
+        description: description.to_string(),
+        completed: false,
+    });
+
+    
+        Ok(())
+    }
+}
+
+
